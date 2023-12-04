4271375a73bfea6c821a17d81d8c6fb7ad34b024
Tomás Caram <tcaramm@gmail.com>
2023-12-04 18:09:28 -0300

    Update index.js

--- web-nfc/index.js
+++ web-nfc/index.js
@@ -10,9 +10,21 @@ scanButton.addEventListener("click", async () => {
       log("Argh! Cannot read data from the NFC tag. Try another one?");
     });
 
-    ndef.addEventListener("reading", ({ message, serialNumber }) => {
-      log(`> Serial Number: ${serialNumber}`);
-      log(`> Records: (${message.records.length})`);
+    message.records.forEach((record, index) => {
+      log(`Record ${index + 1}:`);
+  
+      if (record.recordType === 'text') {
+        const textDecoder = new TextDecoder();
+        const text = textDecoder.decode(record.data);
+        log(`Text Record Data: ${text}`);
+      } else if (record.recordType === 'url') {
+        // Handle URL record type
+        const url = record.data;
+        log(`URL Record Data: ${url}`);
+      } else {
+        // Handle other record types as needed
+        log(`Record Data: ${record.data}`);
+      }
     });
   } catch (error) {
     log("Argh! " + error);
